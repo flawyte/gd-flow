879ec988cee977753f9fba6debb4a6ddb2801e88
Mickaël Allonneau <mickael.allonneau@gmail.com>
2015-03-21 04:16:32 +0100

    tests > added default file 'server/dep_dir/old_file'

--- tests/server/setup.sh
+++ tests/server/setup.sh
@@ -1,6 +1,9 @@
 rm -rf dep_dir/ \
   && echo "'dep_dir/' has been removed"
 
+mkdir dep_dir/ && echo "should not be removed" > dep_dir/old_file \
+  && echo "'dep_dir/old_file' has been created"
+
 rm -rf git_dir/ \
   && echo "'git_dir/' has been removed"
 
